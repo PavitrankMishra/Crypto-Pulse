a0134dc0807a601074eb39be11071e6286419a04
Pavitrank <pavitrank2002@gmail.com>
2025-03-18 23:23:10 +0530

    Developed the initial structure of Rates component with data rendering.

--- src/Components/Rates.jsx
+++ src/Components/Rates.jsx
@@ -6,18 +6,21 @@ const Rates = () => {
   const [rates, setRates] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
   const fetchRates = async () => {
     try {
       setLoading(true);
-      const response = await fetch("https://api.coincap.io/v2/rates");
+      const response = await fetch("https://api.coincap.io/v2/assets");
       console.log(response);
       if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
       }
       const data = await response.json();
-      setRates(data.data.slice(0, 10));
+      setRates(data.data);
       setLoading(false);
+      console.log(data.data);
     } catch (err) {
       setError(err);
     } finally {
@@ -27,10 +30,70 @@ const Rates = () => {
   useEffect(() => {
     fetchRates();
   }, []);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = currentPage * itemsPerPage;
+  const visibleRates = rates.slice(startIndex, endIndex);
   return (
     <div>
       <Navbar />
-      <p>This is the rates component</p>
+      <h2>Crypto Rates</h2>
+      {loading && <p>Loading....</p>}
+
+      {error && <p>Error: {error}</p>}
+
+      {!loading && !error && rates.length > 0 && (
+        <>
+          <table border={1}>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Symbol</th>
+                <th>Price (USD)</th>
+                <th>24H Change</th>
+                <th>Market</th>
+              </tr>
+            </thead>
+            <tbody>
+              {visibleRates.map((coin) => (
+                <tr key={coin.id}>
+                  <td>{coin.rank}</td>
+                  <td>{coin.name}</td>
+                  <td>{coin.symbol}</td>
+                  <td>${parseFloat(coin.priceUsd).toFixed(2)}</td>
+                  <td
+                    style={{
+                      color:
+                        parseFloat(coin.changePercent24Hr) < 0
+                          ? "red"
+                          : "green",
+                    }}
+                  >
+                    {parseFloat(coin.changePercent24Hr).toFixed(2)}%
+                  </td>
+                  <td>{coin.symbol}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div style={{ marginTop: "20px" }}>
+            <button
+              onClick={() => setCurrentPage((prev) => prev - 1)}
+              disabled={currentPage == 1}
+            >
+              Previous
+            </button>
+            <button
+              onClick={() => setCurrentPage((prev) => prev + 1)}
+              disabled={endIndex >= rates.length}
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
