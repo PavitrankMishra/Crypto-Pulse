9f3eac91587e070fa9f6fea036d4cdd4996b575e
Pavitrank <pavitrank2002@gmail.com>
2025-03-18 22:06:58 +0530

    Implemented data fetching from the Rates component and initialised useState for rates, loading and error

--- src/Components/Rates.jsx
+++ src/Components/Rates.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import Navbar from './Navbar';
+import React from "react";
+import Navbar from "./Navbar";
+import { useState, useEffect } from "react";
 
 const Rates = () => {
+  const [rates, setRates] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchRates = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("https://api.coincap.io/v2/rates");
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      const data = await response.json();
+      setRates(data.data.slice(0, 10));
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchRates();
+  }, []);
   return (
     <div>
-    <Navbar />
-        <p>This is the rates component</p>
+      <Navbar />
+      <p>This is the rates component</p>
     </div>
-  )
-}
+  );
+};
 
 export default Rates;
