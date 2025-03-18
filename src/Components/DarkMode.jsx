1c98513bd0593bf02081e4354da918521dcbbc00
Pavitrank <pavitrank2002@gmail.com>
2025-03-18 07:59:50 +0530

    feat: Implement darkmode toggle with theme persistence

--- src/Components/DarkMode.jsx
+++ src/Components/DarkMode.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun } from "@fortawesome/free-solid-svg-icons";
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
-import styles from "./Darkmode.module.css";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import styles from "./DarkMode.module.css";
 
 const DarkMode = () => {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") === "dark"
+  );
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("darkmode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("darkmode");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
+  const toggleTheme = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
   return (
     <div className={styles.darkmode}>
       <input
         className={styles.darkmode_input}
         type="checkbox"
         id="darkmode_toggle"
+        checked={darkMode}
+        onChange={toggleTheme}
       />
       <label className={styles.darkmode_label} htmlFor="darkmode_toggle">
         <FontAwesomeIcon icon={faSun} className={styles.darkmode_label_sun} />
