bd4dd49a020d1a7b540479876b5e34ca9d621673
Pavitrank <pavitrank2002@gmail.com>
2025-03-18 23:42:41 +0530

    Initialised variables for data capturing, loading and error.

--- src/Components/Exchanges.jsx
+++ src/Components/Exchanges.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
 
 const Exchanges = () => {
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchExchanges = async () => {
+    try {
+      const response = await fetch("https://api.coincap.io/v2/exchanges");
+      const data = await response.json();
+      console.log(data.data);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchExchanges();
+  }, []);
   return (
     <div>
       <Navbar />
-      <p>This is the exchanges part</p>
+      <h1>Cryptocurrency Exchanges</h1>
     </div>
   );
 };
