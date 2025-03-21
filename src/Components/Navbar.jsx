5112337ee7315c88e11f281b92d48d4616f03852
Pavitrank <pavitrank2002@gmail.com>
2025-03-21 21:09:31 +0530

    fix: Modified the UI of home page and Exchanges page for error free design.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 // import DarkMode from "./DarkMode";
 
 const Navbar = () => {
@@ -9,10 +10,18 @@ const Navbar = () => {
         <span>CRYPTO</span> PULSE
       </h1>
       <div className={styles.navbarRight}>
-        <h3>Home</h3>
-        <h3>About</h3>
-        <h3>Contact Us</h3>
-        <h3>More</h3>
+        <h3>
+          <Link to="/assets">Assets</Link>
+        </h3>
+        <h3>
+          <Link to="/exchanges">Exchanges</Link>
+        </h3>
+        <h3>
+          <Link to="/livemarket">Live Market</Link>
+        </h3>
+        <h3>
+          <Link to="/historicdata">Historic Data</Link>
+        </h3>
       </div>
       {/* <DarkMode /> */}
     </header>
